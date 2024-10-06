a092e29a0c8a4be5281a3b411fe623f1646de73f
ssup2 <supsup5642@gmail.com>
2024-10-06 15:27:56 +0900

    update

--- scripts/migration_from_jekyll.sh
+++ scripts/migration_from_jekyll.sh
@@ -5,25 +5,27 @@ JEKYLL_MARKDOWN_FILE=$1
 JEKYLL_IMAGE_DIR=$2
 HUGO_BLOG_DIR=$3
 
-HUGO_CONTENT_DIR="$HUGO_BLOG_DIR"
-HUGO_IMAGE_DIR="$HUGO_BLOG_DIR/images"
+HUGO_CONTENT_DIRNAME=$(basename "$JEKYLL_MARKDOWN_FILE" | tr 'A-Z' 'a-z' | tr '_' '-')
+HUGO_CONTENT_PARENT_DIR="$HUGO_BLOG_DIR"
+HUGO_CONTENT_ROOT_DIR="$HUGO_CONTENT_PARENT_DIR/$HUGO_CONTENT_DIRNAME"
+HUGO_CONTENT_IMAGE_DIR="$HUGO_CONTENT_ROOT_DIR/images"
 
 # Copy the Jekyll markdown file (remove lines 3-7, replace "### " with "## ", and replace "[그림 " with "[Figure ")
 echo "Processing markdown file $JEKYLL_MARKDOWN_FILE to Hugo..."
-mkdir -p "$HUGO_CONTENT_DIR"
-awk 'NR < 3 || NR > 7' "$JEKYLL_MARKDOWN_FILE" | sed 's/^### /## /g' | sed 's/\[그림 /\[Figure /g' > "$HUGO_CONTENT_DIR/index.md"
+mkdir -p "$HUGO_CONTENT_IMAGE_DIR"
+awk 'NR < 3 || NR > 7' "$JEKYLL_MARKDOWN_FILE" | sed 's/^### /## /g' | sed 's/\[그림 /\[Figure /g' > "$HUGO_CONTENT_ROOT_DIR/index.md"
 
 # Copy Jekyll image files (only if the image directory is not empty)
 if [ -n "$JEKYLL_IMAGE_DIR" ]; then
     echo "Copying image files from Jekyll to Hugo..."
-    mkdir -p "$HUGO_IMAGE_DIR"
-    cp -r "$JEKYLL_IMAGE_DIR"/* "$HUGO_IMAGE_DIR/"
+    mkdir -p "$HUGO_CONTENT_IMAGE_DIR"
+    cp -r "$JEKYLL_IMAGE_DIR"/* "$HUGO_CONTENT_IMAGE_DIR/"
 
     # After copying, check for any .pptx files and rename them to images.pptx
-    if compgen -G "$HUGO_IMAGE_DIR/*.pptx" > /dev/null; then
-        for pptx_file in "$HUGO_IMAGE_DIR"/*.pptx; do
-            mv "$pptx_file" "$HUGO_IMAGE_DIR/images.pptx"
-            echo "Renamed $pptx_file to images.pptx"
+    if compgen -G "$HUGO_CONTENT_IMAGE_DIR/*.pptx" > /dev/null; then
+        for pptx_file in "$HUGO_CONTENT_IMAGE_DIR"/*.pptx; do
+            mv "$pptx_file" "$HUGO_CONTENT_IMAGE_DIR/images.pptx"
+            echo "Renamed $HUGO_CONTENT_IMAGE_DIR to images.pptx"
         done
     else
         echo "No .pptx file found to rename."
@@ -31,7 +33,7 @@ if [ -n "$JEKYLL_IMAGE_DIR" ]; then
 
     # Rename the files to lowercase and replace underscores with hyphens
     echo "Renaming image files to lowercase and replacing underscores with hyphens..."
-    for img_file in "$HUGO_IMAGE_DIR"/*; do
+    for img_file in "$HUGO_CONTENT_IMAGE_DIR"/*; do
         if [ -f "$img_file" ]; then
             # Get the file path with lowercase and underscores replaced by hyphens
             new_img_file=$(echo "$img_file" | tr 'A-Z' 'a-z' | tr '_' '-')
